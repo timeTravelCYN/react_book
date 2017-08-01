38f30cf6bb3d4292e51938904b9a2f85f9a7c623
timeTravelCYN <554793916@qq.com>
2017-08-01 09:27:57 +0800

    react state...

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -70,9 +70,11 @@ class LikeButton extends Component {
   }
 
   render () {
+    const likedText = this.props.likedText || '取消'
+    const unlikedText = this.props.unlikedText || '点赞'
     return (
       <button onClick={this.handleClickOnLikeButton.bind(this)}>
-        {this.state.isLiked ? '取消' : '点赞'}  👍
+        {this.state.isLiked ? likedText : unlikedText}  👍
       </button>
     )
   }
